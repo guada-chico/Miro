56d8ee91ddc86d40ac38ca1efa162a79e9d95fce
Guada <guadalupe.fernandez@des.kyocera.com>
2026-05-04 15:43:31 +0200

    login visual check

--- frontend_miro/src/App.jsx
+++ frontend_miro/src/App.jsx
@@ -1,20 +1,22 @@
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login/Login'; 
+// IMPORTANTE: 'login' en minúscula para que coincida exactamente con tu carpeta
+import Login from './pages/login/Login'; 
 
 export default function App() {
   const [token, setToken] = useState(localStorage.getItem('token'));
 
-  // IMPORTANTE: Asegúrate de que no haya NADA aquí arriba (como useNavigate)
-  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<div>Si ves esto, el error es el archivo Login.jsx</div>} />
+        <Route path="/login" element={<Login setToken={setToken} />} />
+        
         <Route 
           path="/inicio" 
           element={token ? <div style={{padding: '50px'}}><h1>Bienvenido a Miro</h1></div> : <Navigate to="/login" />} 
         />
+
+        {/* Cualquier otra ruta nos manda al login */}
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
