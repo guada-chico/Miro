bf5532f24cd4a443a80ccf9a6be058c860c2019f
Guada <guadalupe.fernandez@des.kyocera.com>
2026-05-07 12:28:05 +0200

    prueba subida 2

--- Backend/obj/Debug/net8.0/Miro.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Miro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Miro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8d0ca4d1f220029b44d93ccaddcc89948ae799c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a889f8980e93f9074696ac03be136b73a7e298")]
 [assembly: System.Reflection.AssemblyProductAttribute("Miro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Miro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
