df497b1e5cfef4157f6aa2d28de85bc24ec7da8a
juliamuelasr <juliamuelasr@gmail.com>
2026-05-07 09:54:52 +0200

    Delete Backend directory

--- Backend/Dto/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Miro.Dto
-{
-    public class LoginRequest
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
--- Backend/Services/Interfaces/IGoogleBookService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Miro.Models;
-
-namespace Miro.Services.Interfaces
-{
-    public interface IGoogleBookService
-    {
-        Task<IEnumerable<Book>> SearchBooksAsync(string query);
-    }
-}
